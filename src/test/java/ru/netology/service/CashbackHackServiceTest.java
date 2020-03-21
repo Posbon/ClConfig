7e5fdc53efbda9cbeec9b609f10e93a2ef456094
Posbon <89262281906@mail.ru>
2020-03-21 13:56:59 +0300

    homework

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,16 +2,16 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CashbackHackServiceTest {
 
     @Test
-    void remain() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        final int boundary = 1000;
-        int amount = 1000;
+    void expected_result_should_be_zero() {
 
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int boundary = 1000;
+        int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
 
@@ -19,11 +19,11 @@ class CashbackHackServiceTest {
     }
 
     @Test
-    void remain1() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        final int boundary = 1000;
-        int amount = 0;
+    void expected_result_should_be_thousand() {
 
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int boundary = 1000;
+        int amount = 0;
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
 
@@ -31,24 +31,25 @@ class CashbackHackServiceTest {
     }
 
     @Test
-    void remain2() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        final int boundary = 1000;
-        int amount = 999;
+    void expected_result_should_be_one() {
 
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int boundary = 1000;
+        int amount = 999;
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
 
         assertEquals(expected, actual);
     }
-    @Test
-    void remain3() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        final int boundary = 1000;
-        int amount = 500;
 
+    @Test
+    void expected_result_should_be_three_hundred() {
+
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int boundary = 2000;
+        int amount = 700;
         int actual = cashbackHackService.remain(amount);
-        int expected = 500;
+        int expected = 300;
 
         assertEquals(expected, actual);
     }
