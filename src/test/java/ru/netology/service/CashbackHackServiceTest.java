d8d83ea809da11c2f3dcd6903413801cb7fd0f4e
Posbon <89262281906@mail.ru>
2020-03-22 02:09:37 +0300

    homework

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,50 +7,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CashbackHackServiceTest {
 
     @Test
-    void expected_result_should_be_zero() {
-
+    void expectedResultShouldBeZero() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 1000;
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void expected_result_should_be_thousand() {
-
+    void expectedResultShouldBeThousand() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 1000;
         int amount = 0;
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void expected_result_should_be_one() {
-
+    void expectedResultShouldBeOne() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 1000;
         int amount = 999;
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void expected_result_should_be_three_hundred() {
-
+    void expectedResultShouldBeThreeHundred() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int boundary = 2000;
         int amount = 700;
         int actual = cashbackHackService.remain(amount);
         int expected = 300;
-
         assertEquals(expected, actual);
     }
 }
